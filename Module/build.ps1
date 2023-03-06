f2e121a177479150bff2fd766a1b1f100d9e7054
Anthony Watherston <anwather@microsoft.com>
2023-03-06 17:22:31 +1100

    Update build.ps1

--- Module/build.ps1
+++ Module/build.ps1
@@ -40,11 +40,3 @@ $functionNames | Foreach-Object {
 Copy-Item -Path .\Scripts\CloudAdoptionFramework\policyAssignments -Destination .\Module\EnterprisePolicyAsCode -Force -Recurse
 
 (Get-Content -Path .\Module\EnterprisePolicyAsCode\EnterprisePolicyAsCode.psd1) -replace "FunctionsToExport = ''", "FunctionsToExport = @($((gci -Path .\Module\EnterprisePolicyAsCode\functions | Select-Object -ExpandProperty BaseName) | Join-String -Separator "," -DoubleQuote))" | Set-Content .\Module\EnterprisePolicyAsCode\EnterprisePolicyAsCode.psd1
-
-$version = git describe --tags
-
-if ($version) {
-    (Get-Content -Path .\Module\EnterprisePolicyAsCode\EnterprisePolicyAsCode.psd1) -replace "ModuleVersion     = ''", "ModuleVersion     = '$version'" | Set-Content .\Module\EnterprisePolicyAsCode\EnterprisePolicyAsCode.psd1
-}
-
-Write-Output "Publishing module version: $version"
