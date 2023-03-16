a998be75a5138273039e8c0412231c8cd5655086
Anthony Watherston <anwather@microsoft.com>
2023-03-16 15:11:46 +1100

    Feature/module development (#162)
    * Added module build action

---
+++ Module/build.ps1
@@ -0,0 +1,42 @@
+New-Item .\Module\EnterprisePolicyAsCode\internal\functions -ItemType Directory -Force
+New-Item .\Module\EnterprisePolicyAsCode\functions -ItemType Directory -Force
+
+Copy-Item -Path .\Scripts\Helpers\*.ps1 -Destination .\Module\EnterprisePolicyAsCode\internal\functions -Force
+
+if (Test-Path .\Module\EnterprisePolicyAsCode\internal\functions\Add-Helpers.ps1) {
+  Remove-Item .\Module\EnterprisePolicyAsCode\internal\functions\Add-Helpers.ps1
+}
+
+# Deploy Functions
+
+$functionNames = (Get-ChildItem .\Scripts\Deploy\* -File -Include *.ps1).BaseName
+
+$functionNames | Foreach-Object {
+    "function $_ {" | Set-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+    Get-Content .\Scripts\Deploy\$_.ps1 | Where-Object { $_ -notmatch "^\." -and $_ -notmatch "^#Requires" } | Add-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+    "}" | Add-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+}
+
+# Operations
+
+$functionNames = (Get-ChildItem .\Scripts\Operations\* -File -Include *.ps1).BaseName
+
+$functionNames | Foreach-Object {
+    "function $_ {" | Set-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+    Get-Content .\Scripts\Operations\$_.ps1 | Where-Object { $_ -notmatch "^\." -and $_ -notmatch "^#Requires" } | Add-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+    "}" | Add-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+}
+
+# Cloud Adoption Framework
+
+$functionNames = (Get-ChildItem .\Scripts\CloudAdoptionFramework\* -File -Include *.ps1).BaseName
+
+$functionNames | Foreach-Object {
+    "function $_ {" | Set-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+    Get-Content .\Scripts\CloudAdoptionFramework\$_.ps1 | Where-Object { $_ -notmatch "^\." -and $_ -notmatch "^#Requires" } | Add-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+    "}" | Add-Content ".\Module\EnterprisePolicyAsCode\functions\$_.ps1" -Force
+}
+
+Copy-Item -Path .\Scripts\CloudAdoptionFramework\policyAssignments -Destination .\Module\EnterprisePolicyAsCode -Force -Recurse
+
+(Get-Content -Path .\Module\EnterprisePolicyAsCode\EnterprisePolicyAsCode.psd1) -replace "FunctionsToExport = ''", "FunctionsToExport = @($((gci -Path .\Module\EnterprisePolicyAsCode\functions | Select-Object -ExpandProperty BaseName) | Join-String -Separator "," -DoubleQuote))" | Set-Content .\Module\EnterprisePolicyAsCode\EnterprisePolicyAsCode.psd1
