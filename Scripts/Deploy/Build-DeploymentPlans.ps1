7585a163ee7cf3c3ce3d8e41589057d0e3384448
Gjon Gojcevic <gjonn@hotmail.com>
2023-04-20 12:16:10 -0400

    fixed null pacOwnerId in plan file

--- Scripts/Deploy/Build-DeploymentPlans.ps1
+++ Scripts/Deploy/Build-DeploymentPlans.ps1
@@ -146,7 +146,7 @@ if ($exemptionsAreManaged) {
         -deployedExemptions $deployedPolicyResources.policyExemptions `
         -exemptions $exemptions
 }
-
+$pacOwnerId = $pacEnvironment.pacOwnerId
 $timestamp = Get-Date -AsUTC -Format "u"
 $policyPlan = @{
     createdOn            = $timestamp
