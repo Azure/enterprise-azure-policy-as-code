8d1c19a3b3d5521f9d6ab7e0320cff96073a4cf9
Heinrich Gantenbein <6719941+techlake@users.noreply.github.com>
2023-04-19 19:17:07 -0500

    Fix bug #190, minor doc changes (#193)

--- Scripts/Helpers/Build-PolicySetPlan.ps1
+++ Scripts/Helpers/Build-PolicySetPlan.ps1
@@ -190,7 +190,7 @@ function Build-PolicySetPlan {
             policyDefinitions      = $policyDefinitionsFinal
             policyDefinitionGroups = $policyDefinitionGroupsFinal
         }
-        Remove-NullOrEmptyFields $definition
+        Remove-NullOrEmptyFields $definition -nullOnly
         $allDefinitions.policysetdefinitions[$id] = $definition
 
         if ($managedDefinitions.ContainsKey($id)) {
