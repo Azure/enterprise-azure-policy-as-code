ef064f73e1431172da172ed952d6979f768b7e4f
Anthony Watherston <Anthony.Watherston@microsoft.com>
2024-04-08 12:22:05 +1000

    Fixed module build script to include nested scripts

--- Module/build.ps1
+++ Module/build.ps1
@@ -1,7 +1,7 @@
 New-Item .\Module\EnterprisePolicyAsCode\internal\functions -ItemType Directory -Force
 New-Item .\Module\EnterprisePolicyAsCode\functions -ItemType Directory -Force
 
-Copy-Item -Path .\Scripts\Helpers\*.ps1 -Destination .\Module\EnterprisePolicyAsCode\internal\functions -Force -Exclude Add-HelperScripts.ps1
+Get-ChildItem -Path .\Scripts\Helpers\*.ps1 -Recurse -File -Exclude Add-HelperScripts. | Copy-Item -Destination .\Module\EnterprisePolicyAsCode\internal\functions
 
 # Deploy Functions
 
