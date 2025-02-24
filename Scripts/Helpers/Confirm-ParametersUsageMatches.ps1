537a0edc3babdd46c0ec9760d88699605b8cf6a3
Anthony Watherston <anwather@microsoft.com>
2025-02-24 13:04:17 +1100

    createObject edge case and metadata ordering. (#890)
    Co-authored-by: Anthony Watherston <Anthony.Watherston@microsoft.com>

--- Scripts/Helpers/Confirm-ParametersUsageMatches.ps1
+++ Scripts/Helpers/Confirm-ParametersUsageMatches.ps1
@@ -33,15 +33,18 @@ function Confirm-ParametersUsageMatches {
             $definedParameter = $definedParameters.$definedParameterName
         }
 
-        $existingParameterValue = $existingParameter
-        if ($null -ne $existingParameterValue.value) {
+        if ($existingParameter -is [System.Collections.Hashtable]) {
             $existingParameterValue = $existingParameter.value
         }
-        $definedParameterValue = $definedParameter
-        if ($null -ne $definedParameterValue.value) {
-            $definedParameterValue = $definedParameter.value
+        else {
+            $existingParameterValue = $existingParameter
         }
 
+        $definedParameterValue = $definedParameter
+        if (($null -ne $definedParameterValue.value) -and (-not($uniqueKeys -contains "AzurePatchRingmaintenanceConfigurationResourceId"))) {
+            $definedParameterValue = $definedParameter.value
+        }
+        
         if (!(Confirm-ObjectValueEqualityDeep $existingParameterValue $definedParameterValue)) {
             return $false
         }
