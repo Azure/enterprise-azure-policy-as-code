60e02941dc3a303fb1f9eb1d428db345cdbe1028
Anthony Watherston <anwather@microsoft.com>
2026-06-16 07:40:34 +1000

    Aw/issue1335  (#1337)
    * Logic fix for replaced identity
    
    * Update string test
    
    ---------
    
    Co-authored-by: Anthony Watherston <Anthony.Watherston@microsoft.com>

--- Scripts/Helpers/Build-AssignmentIdentityChanges.ps1
+++ Scripts/Helpers/Build-AssignmentIdentityChanges.ps1
@@ -23,7 +23,7 @@ function Build-AssignmentIdentityChanges {
     $definedUserAssignedIdentity = $null
     $requiredRoleAssignments = @()
 
-    $existingLocation = $Existing.location 
+    $existingLocation = if ([string]::IsNullOrEmpty($Existing.location)) { "global" } else { $Existing.location }
     $definedLocation = "global"
     if ($hasExistingIdentity) { 
         $existingIdentityType = $existingIdentity.type 
